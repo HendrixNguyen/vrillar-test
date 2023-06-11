472bf42fd7acc8dcb04a43f184b2a09d8e5dc649
hendrixnguyen <huyndg.itdev@gmail.com>
2023-06-11 13:22:47 +0700

    add restapi

--- scrawling/main.ts
+++ scrawling/main.ts
@@ -1,8 +1,10 @@
 import {crawlRank} from "./rank";
 import * as cheerio from "cheerio";
 import {connectDb} from "../db_connector/schemas";
+import dotenv from 'dotenv'
 
 const main = () => {
+    dotenv.config()
     connectDb().then(_ => console.log('DB has connected')).catch(() => process.exit(0))
     crawlRank().catch((e) => console.log(e))
     setTimeout(() => process.exit(0), 1000 * 60 * 10)
